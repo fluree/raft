bdfe18b5ea41eda5e48478d29af9796da7e6e8fc
bplatz <bplatz@flur.ee>
2018-11-27 13:48:18 -0500

    Statless RAFT improvements

--- dev/user.clj
+++ dev/user.clj
@@ -1,22 +1,11 @@
 (ns user
   (:require [clojure.core.async :as async]
             [clojure.tools.logging :as log]
-            [fluree.raft :as raft]
-            [fluree.raft.config :as config])
+            [fluree.raft :as raft])
   (:import (java.util UUID)))
 
 
-(defn state-machine
-  [state-atom]
-  (fn [[op k v compare]]
-    (case op
-      :write (swap! state-atom assoc k v)
-      :read (get @state-atom k)
-      :delete (swap! state-atom dissoc k)
-      :cas (swap! state-atom (fn [state]
-                               (if (= compare (get state k))
-                                 (assoc state k v)
-                                 state))))))
+
 
 
 (def servers-atom (atom {:a {:chan (async/chan)}
@@ -34,16 +23,24 @@
 
 
 (defn monitor-incoming-rcp
+  "Receives incoming commands from external servers"
   [raft]
-  (let [this-server (config/this-server raft)
+  (let [this-server (:this-server raft)
         c           (get-in @servers-atom [this-server :chan])]
     (async/go-loop []
       (let [rpc (async/<! c)]
         (if (nil? rpc)
           (log/warn "RPC channel closed for server:" this-server)
-          (let [[header data] rpc]
+          (let [[header data] rpc
+                {:keys [op resp-chan]} header
+                resp-header (assoc header :op (keyword (str (name op) "-response"))
+                                          :to (:from header)
+                                          :from (:to header))
+                callback    (fn [x]
+                              (log/debug "OP Callback: " {:op op :request data :response x})
+                              (async/put! resp-chan [resp-header x]))]
             ;(log/info (str this-server " - incoming RPC: ") header data)
-            (raft/invoke-rpc-handler raft header data)
+            (raft/invoke-rpc-handler raft op data callback)
             (recur)))))))
 
 
@@ -53,7 +50,7 @@
   (let [server-config (get @servers-atom server)
         server-chan   (:chan server-config)
         resp-chan     (async/promise-chan)
-        this-server   (config/this-server raft)
+        this-server   (:this-server raft)
         msg-id        (str (UUID/randomUUID))
         header        {:op        operation
                        :from      this-server
@@ -62,23 +59,22 @@
                        :resp-chan resp-chan}]
     (async/put! server-chan [header data])
     ;; wait for response and call back
-    (async/go (let [response (async/<! resp-chan)]
-                #_(log/warn (str this-server " - send rpc " {:header   (select-keys header [:op :from :to])
-                                                           :data     data
-                                                           :response response}))
-                (callback response)))
+    ;; for now a single response channel is created for each request, so we need to match up requests/responses
+    (async/go (let [response (async/<! resp-chan)
+                    [header data] response]
+                (log/warn (str this-server " - send rpc resp: "
+                               {:header   (select-keys header [:op :from :to])
+                                :data     data
+                                :response response}))
+                (callback data)))
     resp-chan))
 
-(def state-machine-atoms {:a (atom {})
-                          :b (atom {})
-                          :c (atom {})})
 
 (defn start
   [server-id]
 
   (let [raft (raft/start {:this-server      server-id
                           :servers          servers
-                          :state-machine    (state-machine (get state-machine-atoms server-id))
                           :election-timeout 6000
                           :broadcast-time   3000
                           :send-rpc-fn      send-rpc
@@ -88,53 +84,44 @@
     raft))
 
 (defn new-command
-  [raft [op k v compare]]
-  (if (= :read op)
-    (let [server-id (get-in raft [:config :this-server])]
-      (-> (get state-machine-atoms server-id) deref (get k)))
-    (raft/add-new-entry raft [op k v compare])))
+  [raft command callback]
+  (let [event-chan (get-in raft [:config :timeout-reset-chan])]
+    (async/put! event-chan [:new-command command callback])))
 
 
 (defn power-load
   "Load a quantity of new keys using specified prefix"
-  [raft prefix quantity]
+  [raft prefix quantity callback]
   (let [entries (map (fn [i] [:write (str prefix "_key_" i) [prefix i]]) (range quantity))]
     (doseq [entry entries]
-      (new-command raft entry))))
+      (new-command raft entry callback))))
+
+
+(defn get-raft-state
+  "Returns raft state via callback function."
+  [raft callback]
+  (let [event-chan (get-in raft [:config :timeout-reset-chan])]
+    (async/put! event-chan [:raft-state nil callback])))
 
 (comment
-
-  (def a (start :a))
-  (def b (start :b))
-  (def c (start :c))
 
   (raft/close a)
   (raft/close b)
   (raft/close c)
 
-  (new-command a [:write "mykey" "myval"])
-  (new-command c [:read "mykey"])
+  (def a (start :a))
+  (def b (start :b))
+  (def c (start :c))
+
+  (get-raft-state a (fn [x] (clojure.pprint/pprint (dissoc x :config))))
+  (get-raft-state b (fn [x] (clojure.pprint/pprint (dissoc x :config))))
+  (get-raft-state c (fn [x] (clojure.pprint/pprint (dissoc x :config))))
+
+  (new-command b [:write "mykey" "myval"] (fn [x] (println "Result: " x)))
+  (new-command b [:read "mykey2"] (fn [x] (println "Result: " x)))
 
   (power-load a "b" 1000)
-  (new-command b [:read "b_key_99"])
-
-
-  )
-
-(comment
-
-  state-machine-atoms
-
-  (-> a
-      :raft-state
-      deref)
-
-  (-> b
-      :raft-state
-      deref)
-
-  (-> c
-      :raft-state
-      deref)
+  (new-command b [:read "b_key_99"] (fn [x] (println "Result: " x)))
 
   )
+
